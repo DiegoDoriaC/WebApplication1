7bf83d8e1359ff8a846d3acefa66c7ac4b978129
DiegoDoria <retrexretrex@hotmail.com>
2024-03-31 18:31:49 -0500

    Aniadiendo la lista como Collection de clase y colocandole el static

--- WebApplication1/Controllers/PersonaController.cs
+++ WebApplication1/Controllers/PersonaController.cs
@@ -7,16 +7,21 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class PersonaController : ControllerBase
     {
-        [HttpGet]
-        public List<Persona> Persona()
-        {
-            List<Persona> p = new List<Persona>
+
+        private static List<Persona> p = new List<Persona>
             {
                 new Persona() { Nombre = "Diego", Edad = 21 },
                 new Persona() { Nombre = "Javier", Edad = 25 },
                 new Persona() { Nombre = "Erica", Edad = 10 },
             };
+
+        [HttpGet]
+        public List<Persona> Persona()
+        {           
             return p;
         }
+
+
+
     }
 }
