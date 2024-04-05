0966a8ae914554248ce3f715e2427843165fd546
DiegoDoria <retrexretrex@hotmail.com>
2024-04-04 21:40:58 -0500

    Apis Generic Terminada

--- WebApplication1/Controllers/PersonaController.cs
+++ WebApplication1/Controllers/PersonaController.cs
@@ -15,7 +15,8 @@ namespace WebApplication1.Controllers
             {
                 new Persona() { Nombre = "Diego", Edad = 21 },
                 new Persona() { Nombre = "Javier", Edad = 25 },
-                new Persona() { Nombre = "Erica", Edad = 10 },
+                new Persona() { Nombre = "Erica", Edad = 09 },
+                new Persona() { Nombre = "Elizabeth", Edad = 15 }
             };
 
         [HttpGet]
@@ -24,13 +25,37 @@ namespace WebApplication1.Controllers
             return p;
         }
 
+        //https://localhost:7126/api/persona/Diego
+        //se manda el parametro a eliminar desde la URL
         [HttpDelete("{nombre}")]
         public ActionResult Delete(string nombre)
         {
             Persona? eliminar = p.Where(p => p.Nombre == nombre).FirstOrDefault();
+            if(eliminar == null) return NoContent();
             p.Remove(eliminar);
             return NoContent();
         }
+
+        //se envia los parametros desde el Body
+        //con el formato JSON
+        [HttpPost]
+        public ActionResult Post([FromBody] Persona persona)
+        {
+            
+            var objeto = persona;
+            p.Add(objeto);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public ActionResult Put(string nombre, [FromBody] Persona persona)
+        {
+            var objeto = persona;
+            p.Where(x => x.Nombre.Equals(nombre));
+            //no hay como hacerlo desde un CollecctionGeneric, esto se hace desde una base de datos
+            return NoContent();
+        }
+
 
 
 
